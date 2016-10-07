eba0b734b525d97e42bdcb01d07ead9b4d0ddfbf
pogithub <poamrong@rith.com>
2016-10-07 12:33:31 -0700

    Updated/added Eric Elliott quote

--- js/product-quotes.js
+++ js/product-quotes.js
@@ -3,20 +3,22 @@
 var quotesJSON = [
   {
     quote: "Don’t give up on programming.",
-    author: "Mark Otto, Product Designer via. GitHub"
+    author: "Mark Otto, Product Designer (GitHub)"
   },
   {
     quote: "Take risks, try new things, and accept that not all the boundary lines between devices, browsers, and the web have been drawn yet.",
-    author: "Luke Wroblewski, Product Designer via. Google"
+    author: "Luke Wroblewski, Product Designer (Google)"
   },
   {
     quote: "The primary goal is to make web application development easier, and in the process to make [the] web become stronger.",
     author: "Miško Hevery, creator of Angular JS"
   },
-  {
-    quote: "Design is shrinking the gap between what a product does and why it exists.",
-    author: "Sahil Lavingia, Founder/CEO via. Gumroad"
-  }
+  // {
+  //   quote: "Design is shrinking the gap between what a product does and why it exists.",
+  //   author: "Sahil Lavingia, Founder/CEO via. Gumroad"
+  // },
+    quote: "Keep dancing.",
+    author: "Eric Elliot, Javascript Guru & Writer"
 ];
 
 // Generate random number between min and max quote count
@@ -28,4 +30,3 @@ document.getElementById("productQuote").innerHTML = "\"" + quotesJSON[rndQuote].
 
 // Quote author
 document.getElementById("productQuoteAuthor").innerHTML = "– " + quotesJSON[rndQuote].author;
-
